89fb71f5b010fefe1ef200f03f516a2a9c4bc07e
OiEiO <809572827@qq.com>
2018-10-10 00:11:17 +0800

    update

---
+++ C-Practical-Training-Courses/Third-class/3.3.1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int a[5][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}, {17, 18, 19, 20}};
+    int x, y;
+    float n;
+    for (x = 0; x < 5; x++) {
+        for (y = 0; y < 4; y++)
+            printf("%3d", a[x][y]);
+        printf("\n");
+    }
+    for (y = 0; y < 4; y++) {
+        n = 0;
+        for (x = 0; x < 5; x++)
+            n += a[x][y];
+        printf("And %.2f for the %dth column.\n", n / 5, y + 1);
+    }
+    return 0;
+}
