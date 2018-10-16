00d22f86a63f609bb8e468d8e72debec982abda5
OiEiO <809572827@qq.com>
2018-10-17 00:42:25 +0800

    update

---
+++ C-Practical-Training-Courses/4_class/4.4.4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+float n = 0;
+float sum();
+int main() {
+    float m;
+    m = sum();
+    printf("2/1+3/2+4/3+5/4+...+50/49=%f\n", m);
+    return 0;
+}
+float sum() {
+    float i;
+    for (i = 1; i < 50; i++)
+        n += (i + 1) / i;
+    return n;
+}
