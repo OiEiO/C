ae0746ed606ff27053d7320d532ae456f993602d
OiEiO <809572827@qq.com>
2018-11-04 15:34:54 +0800

    update

---
+++ C_primer_plus/chapter_2/2.12.6.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    int toes = 10;
+
+    printf("Initial value: %d\nDoubled: %d\nSquare: %d\n", toes, toes * 2, toes * toes);
+    return 0;
+}
---
+++ C_primer_plus/chapter_2/2.12.7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void word();
+
+int main() {
+    word();
+    word();
+    word();
+    printf("\n");
+    word();
+    word();
+    printf("\n");
+    word();
+    return 0;
+}
+
+void word() { printf("Smile!"); }
