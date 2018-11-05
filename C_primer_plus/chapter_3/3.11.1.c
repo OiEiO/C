41e408fc30889ebe0d8e398fee30f4257e57889c
OiEiO <809572827@qq.com>
2018-11-06 00:20:44 +0800

    update

---
+++ C_primer_plus/chapter_3/3.11.1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int x;
+    float y;
+
+    printf("Please enter an integer: ");
+    scanf("%d", &x);
+    printf("%d\n", x);
+
+    printf("Please enter a decimal number: ");
+    scanf("%f", &y);
+    printf("%f", y);
+    return 0;
+}
---
+++ C_primer_plus/chapter_3/3.11.2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int n;
+
+    printf("Please enter an ASCLL code: ");
+    scanf("%d", &n);
+    printf("%c\n", n);
+
+    return 0;
+}
