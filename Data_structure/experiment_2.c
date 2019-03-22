dcba9c7cf91a145a6742581922103cfc915a0e15
OiEiO <809572827@qq.com>
2019-03-22 10:14:50 +0800

    update

--- Data_structure/experiment_2.c
+++ Data_structure/experiment_2.c
@@ -11,6 +11,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define ERROR 0
+#define OK 1
+
 //定义一个学生类型
 typedef struct student {
     char no[10];   //学号
@@ -43,11 +46,11 @@ void Locate(Snode *s);
 //按序号查找
 void Search(Snode *s);
 //插入函数
-
+int Insert(Snode *s);
 //删除函数
-
+int Dle(Snode *s);
 //统计函数
-
+void Sta(Snode *s);
 //清空缓存区
 void empty();
 
@@ -73,15 +76,30 @@ int main() {
             Locate(head);
             break;
         case 4:
+            Search(head);
             break;
         case 5:
+            if (Insert(head)) {
+                printf("\n-----------------\n");
+                printf("     插入成功\n");
+                printf("-----------------\n");
+            }
             break;
         case 6:
+            if (Dle(head)) {
+                printf("\n-----------------\n");
+                printf("     删除成功\n");
+                printf("-----------------\n");
+            }
             break;
         case 7:
+            Sta(head);
             break;
-        case 8:
+        case 0:
             exit = 1;
+            break;
+        default:
+            printf("\n---输入有误---\n");
             break;
         }
         empty();
@@ -94,7 +112,7 @@ int main() {
 
 //菜单界面
 int meau(int num) {
-    printf("=========================================");
+    printf("\n=========================================");
     printf("\n\n\t          菜单界面");
     printf("\n\n\t       1.创建学生表");
     printf("\n\n\t       2.显示学生表");
@@ -103,7 +121,7 @@ int meau(int num) {
     printf("\n\n\t       5.插入学生");
     printf("\n\n\t       6.删除学生");
     printf("\n\n\t       7.统计学生人数");
-    printf("\n\n\t       8.退出学生表");
+    printf("\n\n\t       0.退出学生表");
     printf("\n\n=========================================\n");
     printf("请输入想要执行的操作：");
     scanf("%d", &num);
@@ -138,7 +156,7 @@ void output(Snode *s) {
     Snode *p;
     p = s;
 
-    printf("\t学生表");
+    printf("\n\t学生表");
     printf("\n*********************\n");
     printf("学号\t姓名\t成绩\n");
 
@@ -146,7 +164,6 @@ void output(Snode *s) {
         p = p->next;
         printf("%s\t%s\t%d\n", p->data.no, p->data.name, p->data.score);
     }
-    printf("*********************\n");
 }
 
 //按姓名查找
@@ -159,19 +176,115 @@ void Locate(Snode *s) {
     printf("\n请输入想要查找学生的姓名：");
     scanf("%s", student_name);
 
-    while (p != NULL) {
-        if (p->data.name != student_name) {
-            p = p->next;
-        } else
-            printf("\n--------------------\n");
-        printf("学号\t姓名\t成绩\n");
-        printf("%s\t%s\t%d", p->data.no, p->data.name, p->data.score);
-        printf("--------------------\n");
-    }
+    for (p = s; p != NULL; p = p->next)
+        if (strcmp(student_name, p->data.name) == 0) {
+            printf("\n----学生信息如下----\n");
+            printf("学号\t姓名\t成绩\n");
+            printf("%s\t%s\t%d\n", p->data.no, p->data.name, p->data.score);
+            printf("--------------------\n");
+            return; //？？？？？？？？？？？？？？？
+        }
+    printf("\n----未找到此学生信息----");
 }
 
 //按序号查找
-void Search(Snode *s) {}
+void Search(Snode *s) {
+    Snode *p;
+    p = s;
+    int m, i = 0;
+
+    printf("请输入想要查找的学生表序号：");
+    scanf("%d", &m);
+
+    if (m <= 0)
+        printf("\n----没有该序号----\n");
+
+    while ((p->next != NULL) && (i < m)) {
+        p = p->next;
+        i++;
+    }
+
+    if (m == i) {
+        printf("\n----学生信息如下----\n");
+        printf("学号\t姓名\t成绩\n");
+        printf("%s\t%s\t%d\n", p->data.no, p->data.name, p->data.score);
+        printf("--------------------\n");
+    } else
+        printf("\n----没有该序号----\n");
+}
+
+//插入函数
+int Insert(Snode *s) {
+    Snode *p, *r;
+    p = s;
+
+    int i, j = 0;
+
+    printf("请输入插入学生表的位置：");
+    scanf("%d", &i);
+
+    while (p != NULL && j < i - 1) {
+        p = p->next;
+        j = j + 1;
+    }
+    if (p == NULL || i < 1) {
+        printf("\n----插入位置不合法----\n");
+        return ERROR;
+    }
+
+    r = (Snode *)malloc(sizeof(Snode));
+    printf("请输入新学生信息：\n");
+    printf("学号:");
+    scanf("%s", r->data.no);
+    printf("姓名:");
+    scanf("%s", r->data.name);
+    printf("成绩:");
+    scanf("%d", &r->data.score);
+    r->next = p->next;
+    p->next = r;
+    return OK;
+}
+
+//删除函数
+int Dle(Snode *s) {
+    Snode *p, *r;
+    p = s;
+
+    int i, j = 0;
+    // char n[35];
+
+    printf("请输入想要删除的位置：");
+    scanf("%d", &i);
+
+    while (p->next != NULL && j < i - 1) {
+        p = p->next;
+        j = j + 1;
+    }
+    if (p->next == NULL || i < 1) {
+        printf("\n----删除位置不合法----\n");
+        return ERROR;
+    }
+
+    r = p->next;
+    p->next = r->next;
+    free(r);
+    return OK;
+}
+
+//统计函数
+void Sta(Snode *s) {
+    Snode *p;
+    p = s;
+
+    int j = 0;
+
+    while (p != NULL) {
+        p = p->next;
+        j++;
+    }
+
+    printf("---当前共有%d名学生---", j);
+}
 
 //清空缓存区
 void empty() {
